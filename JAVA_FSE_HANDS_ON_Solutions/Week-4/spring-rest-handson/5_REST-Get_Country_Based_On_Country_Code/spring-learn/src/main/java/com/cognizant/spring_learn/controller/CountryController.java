8f50469380fe7fa0c57ee39f15aff09e6fc5fa5a
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-07-13 15:02:45 +0530

    Week-4 Assignment Completed

---
+++ JAVA_FSE_HANDS_ON_Solutions/Week-4/spring-rest-handson/5_REST-Get_Country_Based_On_Country_Code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.model.Country;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country")
+    public Country getCountryIndia() {
+        return countryService.getCountry("IN"); // returns India
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
